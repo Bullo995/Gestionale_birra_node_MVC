8a70f747c63a2713bc69f51694a36c94713c5c89
bullo995 <nicolabullita@gmail.com>
2023-08-23 11:57:57 +0200

    inizio logica inserimento articoli magazzino

--- public/js/magazzinoArticoli.js
+++ public/js/magazzinoArticoli.js
@@ -1,12 +1,22 @@
-import { categorie, sottocategoria } from "./tool.js";
+import { categorie, sottocategoria,listaArticoli, unitaM, unitaByid } from "./tool.js";
 let caricato = false;
+
+
 
 document.getElementById('invioCollapse')
 .addEventListener('show.bs.collapse', () => {
     
-    const catArticolo = document.getElementById("categoriaAdd");
-    const sottocatArticolo = document.getElementById("sottocategoriaAdd");
+    const catArticoloSel = document.getElementById("categoriaAdd");
+    const sottocatArticoloSel = document.getElementById("sottocategoriaAdd");
+    const articoloSel = document.getElementById("articoloAdd");
+    const unitaArticolo = document.getElementById("unitaArticoloAdd");
+    const fornitoreArticoloSel = document.getElementById("fornitoreArticoloAdd");
 
+    sottocatArticoloSel.disabled = true;
+    articoloSel.disabled = true;
+    unitaArticolo.value = "";
+
+    // non è completo è pieno di bug 
     if(!caricato){
         caricato = true;
         categorie()
@@ -15,22 +25,54 @@ document.getElementById('invioCollapse')
                 const newOption = document.createElement('option');
                 newOption.textContent = categoria.categoria;
                 newOption.value = categoria.id_categoria;
-                catArticolo.appendChild(newOption);
+                catArticoloSel.appendChild(newOption);
             })
         })
-        catArticolo.addEventListener(`change`, ()=>{
+        catArticoloSel.addEventListener(`change`, ()=>{
 
-            sottocategoria(catArticolo.value)
+            sottocategoria(catArticoloSel.value)
             .then(sottocategorie =>{
-                //sottocatArticolo.disabled = false;
-                sottocatArticolo.innerHTML = "";
+                sottocatArticoloSel.disabled = false;
+                sottocatArticoloSel.innerHTML = "";
                 sottocategorie.forEach(sottocategoria =>{
                     const newOption = document.createElement('option');
                     newOption.textContent = sottocategoria.sottocategoria;
                     newOption.value = sottocategoria.id_sottocategoria;
-                    sottocatArticolo.appendChild(newOption);
+                    sottocatArticoloSel.appendChild(newOption);
                 })
             })    
+        })
+        sottocatArticoloSel.addEventListener('change', ()=>{
+
+            listaArticoli(sottocatArticoloSel.value)
+            .then(articoli=>{
+
+                articoloSel.innerHTML = "";
+                articoloSel.disabled = true;
+                unitaArticolo.value = "";
+
+                if(articoli.length > 0){
+                    let idUnita;
+                    articoloSel.disabled = false;
+                    
+                    articoli.forEach(articolo =>{
+                        const newOption = document.createElement('option');
+                        newOption.textContent = articolo.nome_articolo;
+                        newOption.value = articolo.id_articolo;
+                        newOption.dataset.idUnita = articolo.id_unita_misura;
+                        articoloSel.appendChild(newOption);
+                    })
+                    let articoloSelezionato = articoloSel.options[articoloSel.selectedIndex];
+                    //da controllare meglio i problemi di caricamento 
+                    if(articoloSelezionato !== undefined && articoloSelezionato !== ""){
+                        unitaByid(articoloSelezionato.dataset.idUnita)
+                        .then(dati=>{
+                        unitaArticolo.value = dati;
+                        })
+                    }
+                }
+            })
+            //inserire caricamento fornitori 
         })
     }
 })
